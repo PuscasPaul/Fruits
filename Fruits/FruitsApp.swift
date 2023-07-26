951b383841ad606b87bf5a6dc222cc9c3f688065
Paul <puscasspaul@yahoo.com>
2023-07-26 21:18:26 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Puscas Paul on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
